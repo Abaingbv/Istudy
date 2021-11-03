e58fa0d80bab7313f71d8e3bd5b072d97020139b
админ <dzhoroeva_a@auca,kg>
2021-11-03 20:16:48 +0600

    commit Aki

---
+++ app/src/main/java/com/example/istudy/university_2.kt
@@ -0,0 +1,11 @@
+package com.example.istudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class university_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_university2)
+    }
+}
